e13e5d40690443074aa15a18bdf2559028a8d0c8
nhlinh2207 <nguyenhoailinh2207@gmail.com>
2022-08-08 00:41:12 +0700

    add No-Auth

--- src/main/java/com/example/jwtdemo/jwt/jwtConfig/JwtRequestFilter.java
+++ src/main/java/com/example/jwtdemo/jwt/jwtConfig/JwtRequestFilter.java
@@ -37,7 +37,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With, No-Auth");
         response.setHeader("Content-Type", "application/json");
         if (request.getHeader(HttpHeaders.ORIGIN) != null
                 && request.getMethod().equals(HttpMethod.OPTIONS.name())
